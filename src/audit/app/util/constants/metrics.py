1f13a8ec469ab91584e7d3f2a3c526be94802ec0
carlosaumente@gmail.com <c062o@e130-pc22.inet.dkfz-heidelberg.de>
2026-03-12 16:49:52 +0100

    Added mapping for Metrics Reloaded labels

--- src/audit/app/util/constants/metrics.py
+++ src/audit/app/util/constants/metrics.py
@@ -1,6 +1,7 @@
 class Metrics:
     def __init__(self):
         self.metrics = {
+            # AUDIT backend
             "Dice": "DICE",
             "Hausdorff distance": "HAUS",
             "Jaccard": "JACC",
@@ -8,7 +9,35 @@ class Metrics:
             "Precision": "PREC",
             "Specificity": "SPEC",
             "Sensitivity": "SENS",
+
+            # metrics reloaded backend
+            "Number of reference pixels": "numb_ref",
+            "Number of predicted pixels": "numb_pred",
+            "True positives": "numb_tp",
+            "False positives": "numb_fp",
+            "False negatives": "numb_fn",
+            "Accuracy ": "accuracy",
+            "Net Benefit": "nb",
+            "Expected Cost": "ec",
+            "Balanced Accuracy": "ba",
+            "Cohen's kappa": "cohens_kappa",
+            "Positive Likelihood ratio +": "lr+",
+            "Intersection over Union": "iou",
+            "F-beta score": "fbeta",
+            "Dice Score Coefficient": "dsc",
+            "Youden index": "youden_ind",
+            "Matthews Correlation Coefficient": "mcc",
+            "CL Dice": "cldice",
+            "Average Aymmetric Aurface Sistance": "assd",
+            "Boundary IoU": "boundary_iou",
+            "Hausdorff dist.": "hd",
+            "Hausdorff dist. percentile": "hd_perc",
+            "Mean Absolute Surface Distance": "masd",
+            "Normalized Surface Distance": "nsd"
+
         }
+
+
 
         self.orderby = {"Ascending": True, "Descending": False}
 
